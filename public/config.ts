8fc978050cd9d79249a60744b249294c435e7466
raygeeknyc <raygeeknyc@gmail.com>
2025-01-04 00:09:48 -0500

    Fix JSON

--- public/config.ts
+++ public/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   // Your web app's Firebase configuration here
   // See https://firebase.google.com/docs/web/setup#add-sdks-initialize
-  apiKey: "XXXXXXXXXXXXXXXX"
+  apiKey: "XXXXXXXXXXXXXXXX",
   authDomain: "votingforthestars.firebaseapp.com",
   projectId: "votingforthestars",
   storageBucket: "votingforthestars.firebasestorage.app",
