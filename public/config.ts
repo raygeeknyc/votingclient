1c94c61517fc27ea0c0c62a17ddf3b35869ab72c
raygeeknyc <raygeeknyc@gmail.com>
2025-01-03 23:58:54 -0500

    Files w/edlided auth info in config.ts

---
+++ public/config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  // Your web app's Firebase configuration here
+  // See https://firebase.google.com/docs/web/setup#add-sdks-initialize
+  apiKey: "XXXXXXXXXXXXXXXX"
+  authDomain: "votingforthestars.firebaseapp.com",
+  projectId: "votingforthestars",
+  storageBucket: "votingforthestars.firebasestorage.app",
+  messagingSenderId: "XXXXXXXXXXXXXXXX",
+  appId: "XXXXXXXXXXXXXXXXXXXXXX"
+};
